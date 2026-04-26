9e1f624cfc6a10caefbcd63480713e2ba6fc1673
duckchih <Yf9TKrBX5Ook4bTJgf9QtG+duckchih@noreply.cnb.cool>
2026-04-26 21:32:36 +0800

    01

--- exercises/01_insert_sort/01_insert_sort.c
+++ exercises/01_insert_sort/01_insert_sort.c
@@ -8,8 +8,16 @@ typedef struct {
 } Student;
 
 void insertion_sort(Student students[], int n) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    for (int i = 1; i < n; i++) {
+        Student key = students[i];
+        int j = i - 1;
+        // 降序排序：如果前一个元素 score 小于 key.score，就向后移动
+        while (j >= 0 && students[j].score < key.score) {
+            students[j + 1] = students[j];
+            j--;
+        }
+        students[j + 1] = key;
+    }
 }
 
 int main(void) {
